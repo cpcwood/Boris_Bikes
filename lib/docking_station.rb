b4e6ca88a41f6f9042143699ae1888ec9341971c
Vish Mayer <vish.mayer@gmail.com>
2019-12-11 15:59:12 +0000

    chapter 12 - 14

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,34 @@
 require_relative "bike"
 
 class DockingStation
+  attr_reader :bikes
+  attr_reader :capacity
+  def initialize
+    @bikes = []
+    @bikes.append(Bike.new)
+    @capacity = 20
+  end
   def release_bike
+    if @bikes.empty?
+      raise
+    else
     Bike.new
+    end
+ end
+  def dock_bike(bike)
+    if @bikes.count >= @capacity
+       raise
+     else
+       true
+     end
+
   end
 end
+
+
+
+ #docking_station = DockingStation.new
+ #docking_station.bikes.clear
+ #bike = docking_station.release_bike
+
+ # docking_station.dock_bike(bike)
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,9 +3,25 @@ require "docking_station"
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
-  it 'releases working bikes' do
+  it 'releases working bikes when bikes are available' do
     bike = subject.release_bike
     expect(bike).to be_working
   end
-end
+  it 'does not release working bikes when bikes are N/A' do
+    subject.bikes.clear
+    expect{subject.release_bike}.to raise_error
+  end
+  it 'does not dock bikes when bikes are Full' do
+    subject = DockingStation.new
+    (subject.capacity-1).times do
+      subject.bikes.append(Bike.new)
+    end
+    puts subject.bikes.count
+    expect{subject.dock_bike}.to raise_error
+  end
+  it 'it docks bike' do
+    bike = subject.release_bike
+    expect(subject.dock_bike(bike)).to be true
 
+  end
+end
